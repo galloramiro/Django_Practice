88efccf44e0aa026ffd23f56031e14d538e0c114
galloramiro <ramiro.r.gallo@gmail.com>
2019-03-26 17:08:58 -0300

    Se creo la base. Falta hacer editable la parte del portfolio y la portada y agregar la aplicación de contacto

---
+++ webpersonal/core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'core/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'core/about.html'
+
+class ContactPageView(TemplateView):
+    template_name = 'core/contact.html'
