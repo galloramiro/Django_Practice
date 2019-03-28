0dc1269b4d0ffc9bf7946f1cf524c822682a5780
galloramiro <ramiro.r.gallo@gmail.com>
2019-03-28 02:21:58 -0300

    (ok) Ckeditor (ok) Detail View (ok) CRUD Views Create (With error in the image upload) (ok) CRUD Views Edit (ok) CRUD Views Delete (ok) CkEditor on CRUD view (  ) Minix de identificación para las vistas crud (  ) Model Acerca de (  ) Contact App with mail

---
+++ webpersonal/portfolio/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Project
+
+class ProjectForm(forms.ModelForm):
+
+
+    class Meta:
+        model = Project
+        fields = ['title', 'description','image']
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Título'}), 
+            'description':forms.Textarea(attrs={'class':'form-control'}),
+        }
+        labels = {
+            'title':'', 
+            'description':'',
+            'image':'',
+        }
--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Project
+from .forms import ProjectForm
 # Create your views here.
 
 class ProjectListView(ListView):
@@ -14,5 +15,18 @@ class ProjectDetailView(DetailView):
 
 class ProjectCreate(CreateView):
     model = Project
-    fields = ['title', 'description','image']
+    form_class = ProjectForm
+    success_url = reverse_lazy('projects:project-list')
+
+class ProjectUpdate(UpdateView):
+    model = Project
+    form_class = ProjectForm
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        return reverse_lazy('project-detail:update', args=[self.object.id]) + '?ok'
+
+
+class ProjectDelete(DeleteView):
+    model = Project
     success_url = reverse_lazy('projects:project-list')
